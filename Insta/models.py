ef252209b2b024b5a019e6d3df80dfed1c4be303
chenzuoting <chenzuotinglearn@gmail.com>
2020-07-29 23:41:11 -0700

    Finish website development

--- Insta/models.py
+++ Insta/models.py
@@ -6,19 +6,6 @@ from django.urls import reverse
 from imagekit.models import ProcessedImageField
 
 # Create your models here.
-class Post(models.Model):
-    title = models.TextField(blank=True, null=True)
-    image = ProcessedImageField(
-        upload_to='static/images/posts',
-        format='JPEG',
-        options={'quality':100},
-        blank=True,
-        null=True
-    )
-
-    def get_absolute_url(self): #called when new post saved
-        return reverse("post_detail", args=[str(self.id)]) #get url based on name
-
 # Set up a custom user model is recommended. 
 # If there are some needs in the future, it's easier to modify the user model and db, than change from AbstractUser to CustomUser
 class InstaUser(AbstractUser):
@@ -29,3 +16,80 @@ class InstaUser(AbstractUser):
         blank=True,
         null=True
     )
+
+    def get_connections(self):
+        connections = UserConnection.objects.filter(creator=self)
+        return connections
+
+    def get_followers(self):
+        followers = UserConnection.objects.filter(following=self)
+        return followers
+
+    def is_followed_by(self, user):
+        followers = UserConnection.objects.filter(following=self)
+        return followers.filter(creator=user).exists()
+
+    def get_absolute_url(self):
+        return reverse('profile', args=[str(self.id)])
+
+    def __str__(self):
+        return self.username
+
+class UserConnection(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    creator = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friendship_creator_set")
+    following = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friend_set")
+
+    def __str__(self):
+        return self.creator.username + ' follows ' + self.following.username
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='my_posts'
+    )
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        upload_to='static/images/posts',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True
+    )
+
+    def get_like_count(self):
+        return self.likes.count()
+
+    def get_absolute_url(self): #called when new post saved
+        return reverse("post_detail", args=[str(self.id)]) #get url based on name
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        #related_name: define here so that its foreign key object, post, can visit it  with this name 
+        #and get all the connections with current post
+        related_name='likes')
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+
+    class Meta:
+        # Use this in a relation model, determine if we need some unique info 
+        # Combination post and user should be unique
+        unique_together = ("post", "user")
+
+    # Display as string
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' likes ' + self.post.title
+
+    
--- Insta/views.py
+++ Insta/views.py
@@ -1,7 +1,9 @@
+# View get all the requests, and decide to use which model and template
+from annoying.decorators import ajax_request
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
-from Insta.models import Post
+from Insta.models import Post, Like, InstaUser, UserConnection
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -14,9 +16,21 @@ class PostsView(ListView):
     model = Post
     template_name = 'index.html'
 
+    #Default get_queryset will get all the post, overwrite it so that it only get follower's post
+    def get_queryset(self):
+        current_user = self.request.user
+        following = set()
+        for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+            following.add(conn.following)
+        return Post.objects.filter(author__in=following)
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class UserDetailView(DetailView):
+    model = InstaUser
+    template_name = 'user_detail.html'
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
@@ -40,3 +54,22 @@ class SignUp(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'signup.html'
     success_url = reverse_lazy("login")
+
+# Annotation: this function is to response ajax request, no need to render to a template
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
